beb535905aa107c1e5faf79f790e023402d9868a
netChen <2485757827@qq.com>
2026-02-16 14:43:46 +0800

    feat: 初始化框架

---
+++ disruptorFlow/src/main/java/com/chenxiaofei/disruptorflow/DisruptorFlowApplication.java
@@ -0,0 +1,13 @@
+package com.chenxiaofei.disruptorflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisruptorFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisruptorFlowApplication.class, args);
+    }
+
+}
---
+++ disruptorFlow/src/test/java/com/chenxiaofei/disruptorflow/DisruptorFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenxiaofei.disruptorflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisruptorFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
